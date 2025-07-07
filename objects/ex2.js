8386c0cbe7b458357ffba0e4d9265cd0eaa3636c
amichy-dahan <amichyd1@gmail.com>
2025-07-07 17:41:00 +0300

    ex2.objects

---
+++ objects/ex2.js
@@ -0,0 +1,28 @@
+let library = {
+    books :{
+        book1:{
+            title:"once",
+            author :"pitter"
+        },
+         book2:{
+            title:"emet",
+            author :"miriam"
+        },
+         book3:{
+            title:"umberlla",
+            author :"cristin"
+        },
+         book4:{
+            title:"once2",
+            author :"pitter"
+        },
+         book5:{
+            title:"once3",
+            author :"pitter"
+        },
+         book6:{
+            title:"one",
+            author :"pitter"
+        },
+    }
+}
