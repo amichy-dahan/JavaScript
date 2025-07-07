6459a33480f8e8c9d62ddf70e69a56bfa156937b
amichy-dahan <amichyd1@gmail.com>
2025-07-07 14:47:25 +0300

    ex1.uplod if and else

---
+++ Conditional-Statements/ex1.js
@@ -0,0 +1,9 @@
+let age = 17;
+
+if (age > 18){
+    console.log("youe can vote !!")
+}else{
+    console.log("sorry , we check again next year.. !!")
+}
+
+
---
+++ JavaScript.basics/ex1.js
@@ -0,0 +1,9 @@
+(5 > 2) && false //false
+!("knife" === "sword") //true
+(1 < 2) || (-1 > -1) || !false 
+"" //true
+(31 % 5) == "1" //t
+!!true
+"5th Avenue" != "5th Avenue"
+52 !== "52"
+(undefined || null)
