bf8d19a31fb1074c298bf6c7e173fc191f99b8da
amichy-dahan <amichyd1@gmail.com>
2025-07-07 17:37:10 +0300

    upload ex1 obj

---
+++ objects/ex1.js
@@ -0,0 +1,21 @@
+let per1 = {
+    name: "amichy",
+    age: 30 ,
+    city: "kiryat gat",
+}
+let per2 = {
+    name: "amichay",
+    age: 30 ,
+    city: "kiryat gat",
+}
+
+
+
+if(per1.age === per2.age){
+     if(per1.city === per2.city){
+          console.log("Jill wanted to date Robert")
+     }else{
+
+        console.log("Jill wanted to date Robert, but couldn't")
+     }
+} 
