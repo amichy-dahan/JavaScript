03967b1cde7a870c69c3b6626b6c1ec42be32408
amichy-dahan <amichyd1@gmail.com>
2025-07-20 15:26:07 +0300

    Destructuring

---
+++ Destructuring/ex1.js
@@ -0,0 +1,10 @@
+let meatArr = ["beef","chicken"]
+let vegetableArr = ["rabbit","carrots","potatoes","lettuce"]
+
+let [a] = vegetableArr
+console.log(a)
+let y = vegetableArr.filter(a => a != "rabbit")
+console.log(y)
+
+meatArr.push(a);
+console.log(meatArr)
---
+++ Destructuring/ex2.js
@@ -0,0 +1,12 @@
+var firstPiece = { id: 101, name: 'Ofri' }
+
+var seoncdPiece = { country: 'Israel'}
+
+
+
+let { id, name } = firstPiece;
+let { country } = seoncdPiece;
+
+let combined = { id, name, country };
+
+console.log(combined)
